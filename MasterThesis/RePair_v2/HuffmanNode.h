eed2f760df0c99499a78f3f1f4e91de08cc41229
Nicolai Christensen <nic.chr88@gmail.com>
2015-06-02 13:57:14 +0200

    Removed legacy Huffman code.

--- MasterThesis/RePair_v2/HuffmanNode.h
+++ MasterThesis/RePair_v2/HuffmanNode.h
@@ -2,19 +2,11 @@
 class HuffmanNode
 {
 public:
-	unsigned long  symbol;
 	int frequency; //Used
 	string code;
-	HuffmanNode * leftChild = nullptr;
-	HuffmanNode * rightChild = nullptr;
 
 	HuffmanNode();
-	HuffmanNode(unsigned long  symbol, int frequency);	
-	HuffmanNode(
-		unsigned long  symbol, 
-		int frequency, 
-		HuffmanNode *& leftChild, 
-		HuffmanNode *& rightChild);
+	HuffmanNode(int frequency);	
 	~HuffmanNode();
 
 	
