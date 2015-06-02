41ba1ce2b38c17ba4e1fcb48ea7a2f140e0e837f
Nicolai Christensen <nic.chr88@gmail.com>
2015-06-02 14:35:50 +0200

    Removed legacy Huffman code.

--- MasterThesis/RePair_v2/HuffmanNode.cpp
+++ MasterThesis/RePair_v2/HuffmanNode.cpp
@@ -4,31 +4,13 @@
 
 HuffmanNode::HuffmanNode()
 {
-	this->symbol = 0;
 	this->frequency = 0;
-	this->leftChild = nullptr;
-	this->rightChild = nullptr;
 	this->code = "";
 }
 
-HuffmanNode::HuffmanNode(long symbol, int frequency)
+HuffmanNode::HuffmanNode(int frequency)
 {
-	this->symbol = symbol;
 	this->frequency = frequency;
-	this->leftChild = nullptr;
-	this->rightChild = nullptr;
-	this->code = "";
-}
-HuffmanNode::HuffmanNode(
-	long symbol,
-	int frequency,
-	HuffmanNode *& leftChild,
-	HuffmanNode *& rightChild)
-{
-	this->symbol = symbol;
-	this->frequency = frequency;
-	this->leftChild = leftChild;
-	this->rightChild = rightChild;
 	this->code = "";
 }
 
--- MasterThesis/RePair_v2/HuffmanNode.h
+++ MasterThesis/RePair_v2/HuffmanNode.h
@@ -2,19 +2,11 @@
 class HuffmanNode
 {
 public:
-	long symbol;
 	int frequency;
 	string code;
-	HuffmanNode * leftChild = nullptr;
-	HuffmanNode * rightChild = nullptr;
 
 	HuffmanNode();
-	HuffmanNode(long symbol, int frequency);	
-	HuffmanNode(
-		long symbol, 
-		int frequency, 
-		HuffmanNode *& leftChild, 
-		HuffmanNode *& rightChild);
+	HuffmanNode(int frequency);	
 	~HuffmanNode();
 
 	
